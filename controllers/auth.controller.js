be16366db6d1bb08675424f2c7c914658e6249dd
aris valdez <samus45@hotmail.es>
2022-02-08 08:10:31 -0400

    Auth controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,6 +4,12 @@ const User = require('../models/user');
 const login = async (req = request, res = response) => {
   const { email, password } = req.body;
   try {
+    const user = User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ msg: 'User or Password invalid' });
+    }
+
     res.json({ ok: true });
   } catch (ex) {
     console.error(ex);
